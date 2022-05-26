75c9a8d66bb52e000bdbbe5a4574969d027bdbaf
Brian <brianc.boston@gmail.com>
2022-05-26 21:43:09 +0800

    server can now save user messages (without ExpressJS yet)

--- Project1/app.js
+++ Project1/app.js
@@ -3,6 +3,7 @@ const os    = require('os');
 const path  = require('path');
 const http  = require('http');
 const https = require('https');
+const { BADQUERY } = require('dns');
 
 // Whenever our server recieves a request, reqListener() is executed
 // function reqListener(req, res){ }
@@ -10,21 +11,63 @@ const https = require('https');
 
 // same as:     http.createServer(function(req, res){})
 const server =  http.createServer((req, res) => {
-    // reading some info from a request    
-    // console.log(req.url);
-    // console.log(req.method);
-    // console.log(req.headers);
+    // getting some info from a request
+    url = req.url;
+    method = req.method;
 
     // Set the header for our response (here we use HTML, but there are others)
     res.setHeader('Content-Type', 'text/html')
+    
+    // If the url is the home page:
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Home Page</title></head>');
+        res.write('<body><h1>This is the home page</h1></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        res.end();
+        return;
+    }
+
+    // If the url is the message page:
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        
+        // NodeJS parses all requests chunk by chunk
+        // req.on() is an event listener and it allows us to listen to events occuring on our server
+        // A 'data' event is fired every time a new chunk is ready
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            // Retrieve the second part of parsedBody, which is the user's entered message
+            const message = parsedBody.split('=')[1];
+            console.log(parsedBody);
+            console.log(message);        
+
+            // Write the user message into message.txt
+            fs.writeFileSync('message.txt', message);
+        });
+
+        
+        // status code of 302 stands for redirection
+        res.statusCode = 302;
+        // reset the location of the url to the homepage
+        res.setHeader('Location', '/');
+        res.end();
+        return;
+    }
+
+
+    res.setHeader('Content-Type', 'text/html')
     res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
+    res.write('<head><title>Home Page</title></head>');
     res.write('<body><h1>This is not a smart way to output HTML</h1></body>');
     res.write('</html>');
     res.end();
     // You cannot use write() after end() !
-
-    process.exit();
 })
 
 // listen() keeps the server in a loop by constantly listening for requests (until exit() or sth wrong happens)
