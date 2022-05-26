985a38ca0a4c37e6dda10cbbd18f0bd9f4f3cec2
Brian <brianc.boston@gmail.com>
2022-05-26 18:09:47 +0800

    Send output from NodeJS server!

--- Project1/app.js
+++ Project1/app.js
@@ -10,9 +10,24 @@ const https = require('https');
 
 // same as:     http.createServer(function(req, res){})
 const server =  http.createServer((req, res) => {
-    console.log(req);
+    // reading some info from a request    
+    // console.log(req.url);
+    // console.log(req.method);
+    // console.log(req.headers);
+
+    // Set the header for our response (here we use HTML, but there are others)
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>This is not a smart way to output HTML</h1></body>');
+    res.write('</html>');
+    res.end();
+    // You cannot use write() after end() !
+
+    process.exit();
 })
 
+// listen() keeps the server in a loop by constantly listening for requests (until exit() or sth wrong happens)
 // 3000 is the port number we pass in
 server.listen(3000);
 
